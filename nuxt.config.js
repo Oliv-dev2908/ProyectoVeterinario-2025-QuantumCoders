6d2ab2e2f59c1b16641b3195b78fe6d3bcbce979
Oliv-dev2908 <oliverradictos@gmail.com>
2025-10-17 01:45:10 -0400

    Fixes Pinia Render
    Fix de pinia para desplegar render

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     API_KEY: process.env.WASENDER_API_KEY,
     DEVICE_ID: process.env.WASENDER_DEVICE_ID
   },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase','@pinia/nuxt',],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   supabase: {
     redirectOptions: {
       login: '/login',
