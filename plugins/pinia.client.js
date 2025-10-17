7a88deb7141cadc1f4fa50f81315b63f96f06c2b
Oliv-dev2908 <oliverradictos@gmail.com>
2025-10-17 01:51:00 -0400

    Update and rename pinia.cliente.js to pinia.client.js

--- plugins/pinia.client.js
+++ plugins/pinia.client.js
@@ -1,8 +1,11 @@
-// plugins/pinia.client.ts
 import { createPinia } from 'pinia'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const pinia = createPinia()
+  pinia.use(({ store }) => {
+    if (process.server) return
+    store.$devtools = false // evita devtools en SSR
+  })
   nuxtApp.vueApp.use(pinia)
 })
